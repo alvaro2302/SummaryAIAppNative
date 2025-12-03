634bfc50698eaedaea8f74817e1302ce32e30b04
Acuiza <Acuiza@hypernovalabs.com>
2025-12-03 19:56:53 -0400

    Initial Commit

---
+++ SummaryApp/SummaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryAppApp.swift
+//  SummaryApp
+//
+//  Created by Alvaro Cuiza on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SummaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
