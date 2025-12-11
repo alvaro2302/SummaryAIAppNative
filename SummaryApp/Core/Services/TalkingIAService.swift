135b008c4f6c4ad90343e51f763010cc5c091a1f
Acuiza <Acuiza@hypernovalabs.com>
2025-12-11 12:26:10 -0400

    add function startTranscription for start transcription that is uploaded

--- SummaryApp/Core/Services/TalkingIAService.swift
+++ SummaryApp/Core/Services/TalkingIAService.swift
@@ -29,19 +29,19 @@ class TalkingService {
         return try JSONDecoder().decode(UploadRecordReponse.self, from: data)
         
     }
-    private func buildMultipartBody(wavData: Data, boundary: String) -> Data {
-           var body = Data()
-
-           // Archivo de audio
-           body.append("--\(boundary)\r\n".data(using: .utf8)!)
-           body.append("Content-Disposition: form-data; name=\"audio\"; filename=\"audio.wav\"\r\n".data(using: .utf8)!)
-           body.append("Content-Type: audio/wav\r\n\r\n".data(using: .utf8)!)
-           body.append(wavData)
-           body.append("\r\n".data(using: .utf8)!)
-
-           // Cierre
-           body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-
-           return body
-       }
+    func startTranscription(audioURL: String) async throws -> TranscriptRecordResponse {
+        let APIKEY = "API_KEY"
+        let url = URL(string: "https://api.assemblyai.com/v2/transcript")!
+        let body: TranscriptRecord = TranscriptRecord(audioUrl: audioURL, languageDetection: true)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue(APIKEY, forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        return try JSONDecoder().decode(TranscriptRecordResponse.self, from: data)
+    }
 }
